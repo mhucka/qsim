dd4ffd88639a28a65c09b9bf30ab8ef77b97d9a1
mhucka <mhucka@google.com>
2025-06-01 16:49:26 +0000

    wip

--- pybind_interface/GetPybind11.cmake
+++ pybind_interface/GetPybind11.cmake
@@ -5,6 +5,7 @@ FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11
   GIT_TAG "v${MIN_PYBIND_VERSION}"
+  OVERRIDE_FIND_PACKAGE
 )
 FetchContent_GetProperties(pybind11)
 find_package(pybind11 "${MIN_PYBIND_VERSION}" CONFIG)
